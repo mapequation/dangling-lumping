7c73ac42abc07ad13636748df9f28d6937980014
Martin Rosvall <m.rosvall@gmail.com>
2016-07-16 19:50:30 +0200

    Removed tokenize.

--- dangling-lumping.h
+++ dangling-lumping.h
@@ -26,33 +26,6 @@ inline string to_string (const T& t){
 
 enum WriteMode { STATENODES, LINKS, CONTEXTS };
 
-vector<string> tokenize(const string& str,string& delimiters){
-
-	vector<string> tokens;
-
-  // skip delimiters at beginning.
-	string::size_type lastPos = str.find_first_not_of(delimiters, 0);
-
-  // find first "non-delimiter".
-	string::size_type pos = str.find_first_of(delimiters, lastPos);
-
-	while(string::npos != pos || string::npos != lastPos){
-
-    // found a token, add it to the vector.
-		tokens.push_back(str.substr(lastPos, pos - lastPos));
-
-    // skip delimiters.  Note the "not_of"
-		lastPos = str.find_first_not_of(delimiters, pos);
-
-    // find next "non-delimiter"
-		pos = str.find_first_of(delimiters, lastPos);
-	}
-
-	return tokens;
-
-}
-
-
 class StateNode{
 public:
 	StateNode();
